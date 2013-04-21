686cdf8ad2410fbda6b164c320ea77862e281284
Matthias Kramm <kramm@quiss.org>
2013-04-21 04:48:47 -0700

    added seccomp child process

---
+++ seccomp.c
@@ -0,0 +1,113 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <memory.h>
+#include <stdarg.h>
+#include <sys/prctl.h>
+#include <sys/signal.h>
+#include <asm/unistd_32.h>
+
+static ssize_t my_write(int handle, void*data, int length) {
+    if(length < 0)
+        length = strlen(data);
+
+    int ret;
+    asm(
+        "mov %1, %%edx\n" // len
+        "mov %2, %%ecx\n" // message
+        "mov %3, %%ebx\n" // fd
+        "mov %4, %%eax\n" // sys_write
+        "int $0x080\n"
+        : "=ra" (ret)
+        : "m" (length),
+          "m" (data),
+          "m" (handle),
+          "i" (__NR_write)
+        );
+    return ret;
+}
+
+static char* dbg(const char*format, ...)
+{
+    static char buffer[256];
+    va_list arglist;
+    va_start(arglist, format);
+    int length = vsnprintf(buffer, sizeof(buffer), format, arglist);
+    va_end(arglist);
+    my_write(1, buffer, length);
+}
+
+static void _syscall_log(int edi, int esi, int edx, int ecx, int ebx, int eax) {
+    dbg("syscall eax=%d ebx=%d ecx=%d edx=%d esi=%d edi=%d\n", 
+            eax, ebx, ecx, edx, esi, edi
+            );
+}
+
+void (*syscall_log)() = _syscall_log;
+
+static void do_syscall(void) {
+    asm("movl (%%ebp), %%ebp\n" // ignore the gcc prologue
+	"pushl %%eax\n"
+	"pushl %%ebx\n"
+	"pushl %%ecx\n"
+	"pushl %%edx\n"
+	"pushl %%esi\n"
+	"pushl %%edi\n"
+        "call *%0\n"
+	"popl %%edi\n"
+	"popl %%esi\n"
+	"popl %%edx\n"
+	"popl %%ecx\n"
+	"popl %%ebx\n"
+	"popl %%eax\n"
+        "int $0x080\n"
+        : 
+	: "m" (syscall_log)
+        );
+}
+
+static void*old_syscall_handler = (void*)0x12345678;
+
+static void hijack_linux_gate(void) {
+    // all your sys calls are belong to us!
+    asm("mov %%gs:0x10, %%eax\n"
+        "mov %%eax, %0\n"
+
+        "mov %1, %%eax\n"
+        "mov %%eax, %%gs:0x10\n"
+
+        : "=m" (old_syscall_handler)
+        : "r" (&do_syscall)
+        : "eax");
+};
+
+void seccomp_lockdown(int max_memory)
+{
+    init_mem_wrapper(max_memory);
+
+    int ret = prctl(PR_SET_SECCOMP, 1, 0, 0, 0);
+    if(ret) {
+        fprintf(stderr, "could not enter secure computation mode\n");
+        perror("prctl");
+        _exit(1);
+    }
+}
+
+#ifdef TEST_SECCOMP
+int main()
+{
+    dbg("hi %d %d\n", 1, 3);
+
+    my_write(1, "test\n", -1);
+    hijack_linux_gate();
+    my_write(1, "syscalls hijacked\n", -1);
+
+    prctl(PR_SET_SECCOMP, 1, 0, 0, 0);
+
+    my_write(1, "running\n", 8);
+
+    int* m = malloc(1024);
+    return 0;
+}
+#endif
---
+++ seccomp.h
@@ -0,0 +1,4 @@
+#ifndef __seccomp_h__
+#define __seccomp_h__
+void seccomp_lockdown(int max_memory);
+#endif
